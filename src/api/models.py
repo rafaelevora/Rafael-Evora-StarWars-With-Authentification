6b2fe58de013a0093243bd5159adf201e05b08ae
rafaelevora <rafael_evora@yahoo.com.mx>
2025-10-02 15:40:29 +0000

    raf

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,104 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+
 
 db = SQLAlchemy()
-
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    favorite_characters = relationship(
+        "Character",
+        secondary="favorite_characters",
+        backref="favorited_by_users"
+    )
+    favorite_planets = relationship(
+        "Planet",
+        secondary="favorite_planets",
+        backref="favorited_by_users"
+    )
+    favorite_species = relationship(
+        "Species",
+        secondary="favorite_species",
+        backref="favorited_by_users"
+    )
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorite_characters": [character.serialize() for character in self.favorite_characters],
+            "favorite_planets": [planet.serialize() for planet in self.favorite_planets],
+            "favorite_species": [species.serialize() for species in self.favorite_species]
+        }
+favorite_characters = db.Table('favorite_characters',
+                               db.Column('user_id', db.Integer,
+                                         db.ForeignKey('user.id')),
+                               db.Column('character_id', db.Integer,
+                                         db.ForeignKey('character.id'))
+                               )
+favorite_planets = db.Table('favorite_planets',
+                            db.Column('user_id', db.Integer,
+                                      db.ForeignKey('user.id')),
+                            db.Column('planet_id', db.Integer,
+                                      db.ForeignKey('planet.id'))
+                            )
+favorite_species = db.Table('favorite_species',
+                            db.Column('user_id', db.Integer,
+                                      db.ForeignKey('user.id')),
+                            db.Column('species_id', db.Integer,
+                                      db.ForeignKey('species.id'))
+                            )
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(50), nullable=True)
+    eye_color: Mapped[str] = mapped_column(String(50), nullable=True)
+    homeworld: Mapped["Planet"] = relationship(back_populates="characters")
+    homeworld_id: Mapped[int] = mapped_column(
+        ForeignKey("planet.id"), nullable=True)
+    species: Mapped["Species"] = relationship(back_populates="characters")
+    species_id: Mapped[int] = mapped_column(
+        ForeignKey("species.id"), nullable=True)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "homeworld": self.homeworld.name if self.homeworld else None,
+            "species": self.species.name if self.species else None
+        }
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+    characters: Mapped[List["Character"]] = relationship(
+        back_populates="homeworld")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "characters": [character.name for character in self.characters]
+        }
+class Species(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    average_height: Mapped[int] = mapped_column(nullable=False)
+    average_lifespan: Mapped[int] = mapped_column(nullable=False)
+    language: Mapped[str] = mapped_column(String(50), nullable=False)
+    characters: Mapped[List["Character"]] = relationship(
+        back_populates="species")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "average_height": self.average_height,
+            "average_lifespan": self.average_lifespan,
+            "language": self.language,
+            "characters": [character.name for character in self.characters]
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Species, Planet
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,196 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/character', methods=["POST"])
+def add_new_character():
+    body = request.json     # getting (requesting) info from front end
+    new_character = Character()     # declaring a new character variable which will be equal to the Character in models.py (Character structure in models.py)
+    new_character.name = body["name"]
+    new_character.hair_color = body["hair_color"]
+    new_character.eye_color = body["eye_color"]
+    new_character.homeworld_id = body["homeworld_id"]
+    new_character.species_id = body["species_id"]
+
+
+    db.session.add(new_character)   # getting to saving point
+    db.session.commit()     # saves it
+
+    return jsonify(new_character.serialize())       #new_character is originally a python object, .serialize turns this object into a dictionary because you cannot jsonify a python object
+
+
+@api.route('/species', methods=["POST"])
+def add_new_species():
+    body = request.json
+    new_species = Species()
+    new_species.name = body["name"]
+    new_species.average_height = body["average_height"]
+    new_species.average_lifespan = body["average_lifespan"]
+    new_species.language = body["language"]
+
+    db.session.add(new_species)
+    db.session.commit()
+
+    return jsonify(new_species.serialize())
+
+
+@api.route('/planet', methods=["POST"])
+def add_new_planet():
+    body = request.json
+    new_planet = Planet()
+    new_planet.name = body["name"]
+    new_planet.population = body["population"]
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return jsonify(new_planet.serialize())
+
+
+@api.route('/planets', methods=["GET"])
+def get_all_planets():
+    planets = Planet.query.all()        # we're grabing a list of all planets from our database, they start out as planet objects
+    planets_serialized = [planet.serialize() for planet in planets]     # the planets inside the list are still objects and we're turning them into a dictionary
+    return jsonify(planets_serialized)      # we jsonify the new list that is a list of planet dictionaries
+
+@api.route('/characters', methods=["GET"])
+def get_all_characters():
+    characters = Character.query.all()
+    characters_serialized = [character.serialize() for character in characters]
+    return jsonify(characters_serialized)
+
+@api.route('/species', methods=["GET"])
+def get_all_species():
+    species = Species.query.all()
+    species_serialized = [species.serialize() for species in species]
+    return jsonify(species_serialized)
+
+@api.route('/characters/<int:character_id>', methods=["GET"])
+def get_single_character(character_id):
+    character = Character.query.get(character_id)
+    return jsonify(character.serialize())
+
+
+@api.route('/species/<int:species_id>', methods=["GET"])
+def get_single_species(species_id):
+    species = Species.query.get(species_id)
+    return jsonify(species.serialize())
+
+@api.route('planets/<int:planet_id>', methods=["GET"])
+def get_single_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    return jsonify(planet.serialize())
+
+@api.route('/signup', methods=["POST"])
+def create_new_user():
+    body = request.json
+    new_user = User() 
+    new_user.email = body["email"]
+    new_user.password = body["password"]
+    new_user.is_active = True
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.serialize())
+
+@api.route('/log-in', methods=["POST"])
+def log_in_user():
+    body = request.json
+    user = User.query.filter_by(email=body["email"], password=body["password"]).first()
+    if user is None:
+        return jsonify({"msg": "user not found / invalid credentials"}), 401
+    
+    return jsonify(user.serialize()) 
+
+
+
+@api.route('/user/<int:user_id>/favorites/characters/<int:character_id>', methods=["GET"])
+def add_user_favorite_characters(user_id, character_id):
+
+    user = User.query.get(user_id)
+    character = Character.query.get(character_id)
+
+    user.favorite_characters.append(character)
+    db.session.commit()
+
+    return jsonify({"favorite_characters": [element.serialize() for element in user.favorite_characters]})
+    
+
+@api.route('user/favorites/planets', methods=["POST"])
+def add_user_favorite_planets():
+    body = request.json
+    user_id = body["user_id"]
+    planet_id = body["planet_id"]
+
+    user = User.query.get(user_id)
+    planet = Planet.query.get(planet_id)
+
+    user.favorite_planets.append(planet)
+    db.session.commit()
+
+    return jsonify({"favorite_planets": [element.serialize() for element in user.favorite_planets]})
+
+
+@api.route('/user/favorites/species', methods=["POST"])
+def add_user_favorite_species():
+    body = request.json
+    user_id = body["user_id"]
+    species_id = body["species_id"]
+
+    user = User.query.get(user_id)
+    species = Species.query.get(species_id)
+
+    user.favorire_species.append(species)
+    db.session.commit()
+
+    return jsonify({"favorite_species": [element.serialize() for element in user.favorite_species]})
+
+
+@api.route('/user/favorites/characters', methods=["DELETE"])
+def remove_favorite_character():
+    body = request.json
+    user_id = body["user_id"]
+    character_id = body["character_id"]
+
+    user = User.query.get(user_id)
+    character = Character.query.get(character_id)
+
+    user.favorite_characters.remove(character)
+    db.session.commit()
+
+    return jsonify({"favorite_characters": [element.serialize() for element in user.favorite_characters]})
+
+
+@api.route('/user/favorites/species', methods=["DELETE"])
+def remove_favorite_species():
+    body = request.json
+    user_id = body["user_id"]
+    species_id = body["species_id"]
+
+    user = User.query.get(user_id)
+    species = Species.query.get(species_id)
+
+    user.favorite_species.remove(species)
+    db.session.commit()
+
+    return jsonify({"favorite_species": [element.serialize() for element in user.favorite_species]})
+
+@api.route('/user/favorites/planets', methods=["DELETE"])
+def remove_favorite_planets():
+    body = request.json
+    user_id = body["user_id"]
+    planet_id = body["planet_id"]
+
+    user = User.query.get(user_id)
+    planet = Planet.query.get(planet_id)
+
+    user.favorite_planets.remove(planet)
+    db.session.commit()
+
+    return jsonify({"favorite_planets": [element.serialize() for element in user.favorite_planets]})
+
+
+
+
